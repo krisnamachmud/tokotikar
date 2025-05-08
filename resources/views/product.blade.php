51ea79f085eeaa4cae4e6e2ba1a535d4abdbf970
krisnamachmud <139326448+krisnamachmud@users.noreply.github.com>
2025-05-08 14:27:57 +0700

    Co-authored-by: sasa <sasa19-acc@users.noreply.github.com>

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -88,7 +88,7 @@
                                         <img src="img/banner-fruits.jpg" class="img-fluid w-100 rounded" alt="">
                                         <div class="position-absolute"
                                             style="top: 50%; right: 10px; transform: translateY(-50%);">
-                                            <h3 class="text-secondary fw-bold">Jahitan Rapih </h3>
+                                            <h3 class="text-secondary fw-bold">Serbaguna </h3>
                                         </div>
                                     </div>
                                 </div>
